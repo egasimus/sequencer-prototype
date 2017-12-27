b2f98c040653339b28c7c19a6bd579c5d36850cd
Adam Avramov <adam.b.avramov@gmail.com>
2017-12-27 15:39:33 +0200

    tabula rasa

---
+++ config/webpack/dev.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const ROOT = (...args) =>
+  require('path').resolve(__dirname, '..', '..', ...args)
+
+module.exports = {
+  target: 'web',
+  context: ROOT(),
+  entry: ROOT('index.js'),
+  output: { path: ROOT('dist'), filename: 'bundle.js' },
+  devServer: { publicPath: '/', overlay: true },
+  plugins: [ new (require('html-webpack-plugin'))() ],
+}
